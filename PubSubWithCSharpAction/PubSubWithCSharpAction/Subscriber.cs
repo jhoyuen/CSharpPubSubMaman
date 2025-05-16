479a9bc50838fc07f906990e5f86281097bc001e
Jonathan Lam <jonathan.lam@innerrange.com>
2025-05-16 13:31:03 +1000

    JPM-8: Add PubSubWithCSharpAction

---
+++ PubSubWithCSharpAction/PubSubWithCSharpAction/Subscriber.cs
@@ -0,0 +1,9 @@
+﻿namespace PubSubWithCSharpAction;
+
+public class Subscriber
+{
+    public void OnNotificationReceived(string message)
+    {
+        Console.WriteLine($"Received and processing message: {message}");
+    }
+}
