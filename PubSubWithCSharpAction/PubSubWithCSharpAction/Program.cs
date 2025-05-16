a5a157a782b7017dd34ef891fa014405cf1503ee
jhoyuen <jon_lg83@yahoo.com>
2025-05-16 13:32:13 +1000

    JPM-8: Add PubSubWithCSharpAction (#8)
    Co-authored-by: Jonathan Lam <jonathan.lam@innerrange.com>

---
+++ PubSubWithCSharpAction/PubSubWithCSharpAction/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PubSubWithCSharpAction;
+
+Console.WriteLine("Hello, World!");
+
+Publisher publisher = new Publisher();
+Subscriber subscriber = new Subscriber();
+Subscriber subscriber2 = new Subscriber();
+
+// Subscribe the subscribers
+publisher.NotificationEvent += subscriber.OnNotificationReceived; 
+publisher.NotificationEvent += subscriber2.OnNotificationReceived;
+
+publisher.PublishMessage("Hello, subscribers!"); // Publish a message
+
+Console.ReadKey();
---
+++ PubSubWithCSharpAction/PubSubWithCSharpAction/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace PubSubWithCSharpAction;
+
+public class Publisher
+{
+    public event Action<string>? NotificationEvent;
+
+    public void PublishMessage(string message)
+    {
+        NotificationEvent?.Invoke(message);
+    }
+}
